64b2370b3a281c630033f3d10de4902f7cd2e1ce
Valerio Cosentino <valcos@bitergia.com>
2018-11-05 11:48:24 +0100

    [archimedes] Initial commit
    Welcome Archimedes!

---
+++ README.md
@@ -0,0 +1,30 @@
+# Archimedes
+
+Archimedes is a tool for managing Kibana/Kibiter dashboards from the command line. It is a part of [GrimoireLab](https://grimoirelab.github.io) platform.
+
+## Requirements
+
+TODO
+
+
+## Installation
+
+TODO
+
+
+## Documentation
+
+TODO
+
+
+## Examples
+
+TODO
+
+## Running tests
+
+TODO
+
+## License
+
+Licensed under GNU General Public License (GPL), version 3 or later.
---
+++ setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2015 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#     Valerio Cosentino <valcos@bitergia.com>
+#
+
+import codecs
+import os
+import re
+import sys
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_md = os.path.join(here, 'README.md')
+version_py = os.path.join(here, 'archimides', '_version.py')
+
+# Pypi wants the description to be in reStrcuturedText, but
+# we have it in Markdown. So, let's convert formats.
+# Set up thinkgs so that if pypandoc is not installed, it
+# just issues a warning.
+try:
+    import pypandoc
+    long_description = pypandoc.convert(readme_md, 'rst')
+except (IOError, ImportError):
+    print("Warning: pypandoc module not found, or pandoc not installed. " +
+          "Using md instead of rst", file=sys.stderr)
+    with codecs.open(readme_md, encoding='utf-8') as f:
+        long_description = f.read()
+
+with codecs.open(version_py, 'r', encoding='utf-8') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(name="archimedes",
+      description="Manage Kibana/Kibiter dashboards from command line",
+      long_description=long_description,
+      url="https://github.com/chaoss/grimoirelab-archimedes",
+      version=version,
+      author="Bitergia",
+      author_email="grimoirelab-discussions@lists.linuxfoundation.org",
+      license="GPLv3",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          'License :: OSI Approved :: ' +
+          'GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5'],
+      keywords="development repositories analytics",
+      packages=['archimedes'],
+      python_requires='>=3.4',
+      setup_requires=['wheel'],
+      extras_require={},
+      tests_require=[],
+      test_suite='tests',
+      install_requires=[
+          'requests>=2.7.0',
+          'grimoirelab-panels>=0.0.7',
+          'grimoirelab-toolkit>=0.1.4'
+      ],
+      scripts=["bin/archimedes"],
+      include_package_data=True,
+      zip_safe=False
+      )
